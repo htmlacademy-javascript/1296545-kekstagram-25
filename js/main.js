75109baca3821e0b20cd71113e48092601201220
Светлана <sve-bashmakova@ya.ru>
2022-02-22 14:37:30 +0300

    js/main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 function getRandomInt(min, max) {
-if(min < 0) {
- return 0;
-}
-if(min === max) {
-return 0;
-}
-if (min > max) {
-return 0;
+if (min < 0 || min === max || min > max){
+    throw new Error('Переданные числа некорректны');
 }
 return Math.floor(Math.random() * (max - min + 1)) + min;
 }
