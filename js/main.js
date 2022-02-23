f9dc77a66616554bb389ccd731cdf645448d2f51
Светлана <sve-bashmakova@ya.ru>
2022-02-23 11:12:32 +0300

    изменения в написании функции

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-function getRandomInt (min, max) {
-  if (min < 0 || min === max || min > max){
+const getRandomInt = (min, max) =>
+    if (min < 0 || min === max || min > max){
     throw new Error('Переданные числа некорректны');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -8,6 +8,4 @@ getRandomInt(2, 5);
 
 
 const checkStringLength = (str, maxLetngth) => str.length <= maxLetngth;
-
-
 checkStringLength('Hello man', 9);
