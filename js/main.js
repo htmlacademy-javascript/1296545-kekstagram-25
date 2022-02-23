f2590c429641d742728083b0eb7d356b5c578f77
Светлана <sve-bashmakova@ya.ru>
2022-02-23 11:19:34 +0300

    исправление 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const getRandomInt = (min, max) =>
-    if (min < 0 || min === max || min > max){
+const getRandomInt = (min, max) => {
+  if (min < 0 || min === max || min > max){
     throw new Error('Переданные числа некорректны');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
