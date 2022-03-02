225a59b9535a980d33aad2ae0677378c2dabdf5b
Светлана <sve-bashmakova@ya.ru>
2022-03-02 15:06:55 +0300

    added object creation

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ getRandomInt(2, 5);
 
 const checkStringLength = (str, maxLength) => str.length <= maxLength;
 checkStringLength('Hello man', 9);
+
+const generatOb = (obId) => {
+  const obKeks = {
+    id:obId,
+    url:accepFoto(obId),
+    description: 'описание фотографии',
+    likes:getRandomInt(15, 20),
+  };
+  return obKeks;
+};
+
+const accepFoto = (n) => 'photos/' + n.toString() + '.jpg';
+
+const createArr = (n) => {
+  const arr1 = [];
+  for (let i = 0; i <= n; i++) {
+    arr1[i] = generatOb(i + 1);
+  };
+  return arr1;
+};
+console.log(createArr(25));
+
+
