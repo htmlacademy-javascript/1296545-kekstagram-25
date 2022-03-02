abbf51c9e471b36d393e1b550c9ed22480962b3a
Светлана <sve-bashmakova@ya.ru>
2022-03-02 17:54:27 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -6,21 +6,58 @@ const getRandomInt = (min, max) => {
 };
 getRandomInt(2, 5);
 
-
 const checkStringLength = (str, maxLength) => str.length <= maxLength;
 checkStringLength('Hello man', 9);
+
+const ARRAY_TEXT_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const UREY_USER_NAME = [
+  'Мария',
+  'Светлана',
+  'Сергей',
+  'Александр',
+  'Дмитрий',
+  'Людмила',
+  'Валерия',
+  'Иван',
+  'Семен',
+];
+
+const PHOTO_DESCRIPTION = [
+  'Передо мной интересная (удачная, занимательная и т.п.) фотография.',
+  'Давайте рассмотрим изображение внимательнее.',
+  'Я считаю, что снимок получился (смешным, занимательным, удачным, завораживающим и т.д.)',
+  'Мне понравилась фотография, потому что она (четко передает чувства, эмоции, атмосферу).',
+];
+
+const createRandomAnnouncement = (_elem, id) => ({
+  id: String(++id).padStart(2, '0'),
+  avatar: 'img/avatar-'+ getRandomInt(1, 6).toString() + '.svg',
+  message: ARRAY_TEXT_COMMENTS[getRandomInt(0, 6)],
+  name: UREY_USER_NAME[getRandomInt(0, 8)],
+});
+
+const accepFoto = (n) => 'photos/' + n.toString() + '.jpg';
 
 const generatOb = (obId) => {
   const obKeks = {
     id:obId,
     url:accepFoto(obId),
-    description: 'описание фотографии',
+    description: PHOTO_DESCRIPTION[getRandomInt(0, 4)],
     likes:getRandomInt(15, 20),
+    comments: Array.from({
+      length: getRandomInt(1, 6),
+    }, createRandomAnnouncement),
   };
   return obKeks;
 };
-
-const accepFoto = (n) => 'photos/' + n.toString() + '.jpg';
 
 const createArr = (n) => {
   const arr1 = [];
@@ -29,21 +66,4 @@ const createArr = (n) => {
   };
   return arr1;
 };
-console.log(createArr(25));
-
-
-const SIMILAR_ANNOUNCEMENT_COUNT = 10;
-
-const createRandomAnnouncement = (_elem, id) => ({
-  id: String(++id).padStart(2, '0'),
-  avatar: 'img/avatar-'+ getRandomInt(1, 6).toString() + '.svg',
-  message: 'В целом всё неплохо. Но не всё.',
-  name: 'Артём',
-});
-
-console.log(
-  Array.from({
-    length: SIMILAR_ANNOUNCEMENT_COUNT,
-  }, createRandomAnnouncement),
-);
-
+console.log(createArr(10));
