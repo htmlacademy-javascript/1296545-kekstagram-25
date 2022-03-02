126a761bc1df870daecfee387d6160e17b497338
Keks <keks@htmlacademy.ru>
2022-03-02 19:48:30 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,80 @@
+const getRandomInt = (min, max) => {
+  if (min < 0 || min === max || min > max){
+    throw new Error('Переданные числа некорректны');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+getRandomInt(2, 5);
+
+
+const checkStringLength = (str, maxLength) => str.length <= maxLength;
+checkStringLength('Hello man', 9);
+const getRandomInt = (min, max) => {
+  if (min < 0 || min === max || min > max){
+    throw new Error('Переданные числа некорректны');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+getRandomInt(2, 5);
+
+const checkStringLength = (str, maxLength) => str.length <= maxLength;
+checkStringLength('Hello man', 9);
+
+const ARRAY_TEXT_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const UREY_USER_NAME = [
+  'Мария',
+  'Светлана',
+  'Сергей',
+  'Александр',
+  'Дмитрий',
+  'Людмила',
+  'Валерия',
+  'Иван',
+  'Семен',
+];
+
+const PHOTO_DESCRIPTION = [
+  'Передо мной интересная (удачная, занимательная и т.п.) фотография.',
+  'Давайте рассмотрим изображение внимательнее.',
+  'Я считаю, что снимок получился (смешным, занимательным, удачным, завораживающим и т.д.)',
+  'Мне понравилась фотография, потому что она (четко передает чувства, эмоции, атмосферу).',
+];
+
+const createRandomAnnouncement = (_elem, id) => ({
+  id: String(++id).padStart(2, '0'),
+  avatar: 'img/avatar-'+ getRandomInt(1, 6).toString() + '.svg',
+  message: ARRAY_TEXT_COMMENTS[getRandomInt(0, 6)],
+  name: UREY_USER_NAME[getRandomInt(0, 8)],
+});
+
+const accepFoto = (n) => 'photos/' + n.toString() + '.jpg';
+
+const generatOb = (obId) => {
+  const obKeks = {
+    id:obId,
+    url:accepFoto(obId),
+    description: PHOTO_DESCRIPTION[getRandomInt(0, 4)],
+    likes:getRandomInt(15, 20),
+    comments: Array.from({
+      length: getRandomInt(1, 6),
+    }, createRandomAnnouncement),
+  };
+  return obKeks;
+};
+
+const createArr = (n) => {
+  const arr1 = [];
+  for (let i = 0; i <= n; i++) {
+    arr1[i] = generatOb(i + 1);
+  };
+  return arr1;
+};
+console.log(createArr(10));
