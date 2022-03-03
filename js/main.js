32c2b40a8cf03bb8fd6b6364323b05e9b43241ad
Светлана <sve-bashmakova@ya.ru>
2022-03-03 11:23:19 +0300

    fix getRandomInt

--- js/main.js
+++ js/main.js
@@ -9,16 +9,6 @@ getRandomInt(2, 5);
 
 const checkStringLength = (str, maxLength) => str.length <= maxLength;
 checkStringLength('Hello man', 9);
-const getRandomInt = (min, max) => {
-  if (min < 0 || min === max || min > max){
-    throw new Error('Переданные числа некорректны');
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-getRandomInt(2, 5);
-
-const checkStringLength = (str, maxLength) => str.length <= maxLength;
-checkStringLength('Hello man', 9);
 
 const ARRAY_TEXT_COMMENTS = [
   'Всё отлично!',
