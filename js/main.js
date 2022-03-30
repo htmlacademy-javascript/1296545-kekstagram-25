9a1828d7a49dc49b2d1486d140f72e0919b9e086
Светлана <sve-bashmakova@ya.ru>
2022-03-30 09:52:30 +0300

    fix main

--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@ import {createDescriptionPhotos, NUMBER_PHOTO_OBJECTS} from './data.js';
 
 createDescriptionPhotos(NUMBER_PHOTO_OBJECTS);
 
+fullScreen(images[0]);
+import {fullScreen} from './fullscreen.js';
