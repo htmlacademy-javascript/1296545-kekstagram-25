b1dc9c2141d4314cd9feaaa5a4f84e1ff121e331
Keks <keks@htmlacademy.ru>
2022-04-06 08:47:10 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/thumbnail.js
@@ -0,0 +1,23 @@
+import {viewingFullScreen} from './fullscreen.js';
+
+export const createTemplatePicture = (images) => {
+  const wrapper =  document.querySelector('#picture');
+  const template = wrapper.content.querySelector('.picture');
+  const pictures = document.querySelector('.pictures');
+  const fragment = document.createDocumentFragment();
+
+  images.forEach((image) => {
+    const element = template.cloneNode(true);
+    element.querySelector('.picture__img').src = image.url;
+    element.querySelector('.picture__likes').textContent = image.likes;
+    element.querySelector('.picture__comments').textContent = image.comments.length;
+
+    element.addEventListener('click', () => {
+      viewingFullScreen(image);
+    });
+
+    fragment.appendChild(element);
+  });
+
+  pictures.appendChild(fragment);
+};
