90163f694997674c1a263d2202460040c5ff6d02
Светлана <sve-bashmakova@ya.ru>
2022-04-12 23:28:58 +0300

    filters

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 import { createTemplatePicture } from './thumbnail.js';
 import { getData } from './load.js';
 import { showAlert } from './util.js';
+import {openFilters} from './filters.js';
 import './form.js';
 
-getData(createTemplatePicture, showAlert);
+getData((images) => {
+  createTemplatePicture(images);
+  openFilters(images);
+}, showAlert);
